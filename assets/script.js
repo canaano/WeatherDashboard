334fa99f4dd15af2b3ce94a3ce21ecb53f3e9a2a
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-11-02 11:33:33 -0700

    added API link and Keys. added id's to cards for data needed

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// API url for openweathermap
+const api_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={APIkey}'
+// API key 
+const APIkey = 'c557c62e75b66b19afd499c178c60c24'
+
+// 5day forecast url for openweathermap
+const forecast_url = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}'
