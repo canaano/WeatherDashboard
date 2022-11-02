fc824708a0b116e1d05c6e846821701a2fd38a6e
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-11-02 16:24:58 -0700

    added JS logic for data fetching

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,57 @@
 // API url for openweathermap
-const api_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={APIkey}'
+const API_URL = 'https://api.openweathermap.org/data/3.0/onecall'
 // API key 
-const APIkey = 'c557c62e75b66b19afd499c178c60c24'
-
+const API_KEY = 'c557c62e75b66b19afd499c178c60c24'
+const SEARCH_API = 'https://api.openweathermap.org/geo/1.0/direct'
 // 5day forecast url for openweathermap
-const forecast_url = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIkey}'
+// const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast'
+const FORECAST_URL = 'https://api.openweathermap.org/data/3.0/onecall'
+
+async function searchForCity(cityName) {
+    const request = await fetch(`${SEARCH_API}?q=${cityName}&appid=${API_KEY}`)
+    const response = await request.json();
+    const { lat, lon } = response[0];
+    return { lat, lon };
+}
+
+async function getForecast(lat, lon) {
+    const request = await fetch(`${FORECAST_URL}?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${API_KEY}`)
+    const response = await request.json();
+    return response
+}
+
+function getHeaderData(city, forecast) {
+    return {
+        city,
+        temp: forecast?.current?.temp,
+        humidity: forecast?.current?.humidity,
+        windSpeed: forecast?.current?.wind_speed,
+    }
+}
+
+function getBodyData(forecast) {
+    return forecast.daily.map((day, i) => {
+      return {
+        temp: day?.temp.day,
+        humidity: day?.humidity,
+        windSpeed: day?.wind_speed,
+        weather: day?.weather
+      }
+    }).filter((_, i) => i <= 4);
+}
+
+$( document ).ready(function() {
+    // Handler for .ready() called.
+    $( "#search-button" ).on( "click", async function() {
+        const cityName = $('#search-input').val()
+        const  { lat, lon} = await searchForCity(cityName);
+        const forecast = await getForecast(lat, lon);
+        // console.log(getHeaderData(cityName, forecast))
+        // console.log(getBodyData(forecast), 'body')
+
+
+        // console.log(forecast, 'raw')
+      })
+  });
+
+//   https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
