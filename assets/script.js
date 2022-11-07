13adea53d45e19183b40c5a1c2e3baec72b0aeb3
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-11-06 20:44:03 -0400

    refactor, added search logic for local storage and icons

--- assets/script.js
+++ assets/script.js
@@ -1,81 +1,151 @@
-// API url for openweathermap
-const API_URL = 'https://api.openweathermap.org/data/3.0/onecall'
-// API key 
-const API_KEY = 'c557c62e75b66b19afd499c178c60c24'
-const SEARCH_API = 'https://api.openweathermap.org/geo/1.0/direct'
-// 5day forecast url for openweathermap
-// const FORECAST_URL = 'https://api.openweathermap.org/data/2.5/forecast'
-const FORECAST_URL = 'https://api.openweathermap.org/data/3.0/onecall'
+const API_URL = 'https://api.openweathermap.org/data/3.0/onecall';
+const API_KEY = 'c557c62e75b66b19afd499c178c60c24';
+const SEARCH_API = 'https://api.openweathermap.org/geo/1.0/direct';
+const FORECAST_URL = 'https://api.openweathermap.org/data/3.0/onecall';
 
-async function searchForCity(cityName) {
-    const request = await fetch(`${SEARCH_API}?q=${cityName}&appid=${API_KEY}`)
-    const response = await request.json();
-    const { lat, lon } = response[0];
-    return { lat, lon };
-}
-
-async function getForecast(lat, lon) {
-    const request = await fetch(`${FORECAST_URL}?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${API_KEY}`)
-    const response = await request.json();
-    return response
-}
-
-function getHeaderData(city, forecast) {
-    return {
-        city,
-        temp: forecast?.current?.temp,
-        humidity: forecast?.current?.humidity,
-        windSpeed: forecast?.current?.wind_speed,
+$(document).ready(function () {
+  function getWeatherIcon(weather) {
+    switch (weather) {
+      case 'Clear':
+        return '<iconify-icon icon="wi:day-sunny"></iconify-icon>';
+      case 'Clouds':
+        return '<iconify-icon icon="bi:cloudy-fill"></iconify-icon>';
+      case 'Rain':
+      case 'Drizzle':
+      case 'Mist':
+        return '<iconify-icon icon="bi:cloud-rain"></iconify-icon>';
+      case 'Thunderstorm':
+        return '<iconify-icon icon="bi:cloud-lightning-rain"></iconify-icon>';
+      case 'Snow':
+        return '<iconify-icon icon="bi:cloud-snow"></iconify-icon>';
+      default:
+        return '<iconify-icon icon="wi:day-sunny"></iconify-icon>';
     }
-}
+  }
+  function getSearchHistory() {
+    const searchHistory = localStorage.getItem('searchHistory')
+      ? JSON.parse(localStorage.getItem('searchHistory'))
+      : [];
+    const dedupedSearchHistory = [...new Set(searchHistory)];
+    return dedupedSearchHistory;
+  }
 
-function getBodyData(forecast) {
-    return forecast.daily.map((day, i) => {
-      return {
-        temp: day?.temp.day,
-        humidity: day?.humidity,
-        windSpeed: day?.wind_speed,
-        weather: day?.weather
+  function setSearchHistory(cityName) {
+    const searchHistory = getSearchHistory();
+    searchHistory.push(cityName);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+  }
+
+  async function searchForCity(cityName) {
+    try {
+      const request = await fetch(
+        `${SEARCH_API}?q=${cityName}&appid=${API_KEY}`,
+      );
+      if (request.ok) {
+        const response = await request.json();
+        const { lat, lon } = response[0];
+        setSearchHistory(cityName);
+        return { lat, lon };
       }
-    }).filter((_, i) => i <= 4);
-}
-
-$( document ).ready(function() {
-    function mountHeaderData(data) {
-      $('#city').text(data.city);
-      $('#temp').text(data.temp);
-      $('#humid').text(data.humidity);
-      $('#wind').text(data.windSpeed);
+    } catch (error) {
+      return { error: 'City not found' };
     }
+  }
 
-    function mountBodyData(data) {
-        const cards = document.getElementsByClassName('forecast-card')
-        $('.forecast-card').addClass('show');
+  async function getForecast(lat, lon) {
+    const request = await fetch(
+      `${FORECAST_URL}?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly,alerts&appid=${API_KEY}`,
+    );
+    const response = await request.json();
+    return response;
+  }
 
-        for(let i = 0; i <= cards.length; i++) {
-          cards[i].innerHTML = `
+  function getHeaderData(city, forecast) {
+    return {
+      city,
+      temp: forecast?.current?.temp,
+      humidity: forecast?.current?.humidity,
+      windSpeed: forecast?.current?.wind_speed,
+      weather: forecast?.current?.weather[0]?.main,
+    };
+  }
+
+  function getBodyData(forecast) {
+    return forecast.daily
+      .map((day, i) => {
+        return {
+          temp: day?.temp.day,
+          humidity: day?.humidity,
+          windSpeed: day?.wind_speed,
+          weather: day?.weather,
+        };
+      })
+      .filter((_, i) => i <= 4);
+  }
+
+  function mountHeaderData(data) {
+    $('#weathernow-icon').html(getWeatherIcon(data.weather[0].main));
+    $('#city').text(data.city);
+    $('#temp').text(data.temp);
+    $('#humid').text(data.humidity);
+    $('#wind').text(data.windSpeed);
+  }
+
+  function mountBodyData(data) {
+    const cards = document.getElementsByClassName('forecast-card');
+    $('.forecast-card').addClass('show');
+
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].innerHTML = `
           <div class="content-wrapper">
-          <img id="current-pic" alt="">
+          <span>
+          ${getWeatherIcon(data[i].weather[0].main)}
+          <span class="forecast-temp">${data[i].weather[0].main}</span>
+          </span>
           <p class="temperature">Temp: ${data[i].temp}</p>
           <p class="humidity">Humidity: ${data[i].humidity}</p>
           <p class="wind-speed">Wind Speed: ${data[i].windSpeed}</p> 
           </div>
-          `
-        }
+          `;
     }
-    // Handler for .ready() called.
-    $( "#search-button" ).on( "click", async function() {
-        const cityName = $('#search-input').val()
-        const  { lat, lon} = await searchForCity(cityName);
-        const forecast = await getForecast(lat, lon);
-        const headerData = getHeaderData(cityName, forecast)
-        const bodyData = getBodyData(forecast);
-        
-        // mount headerData on page 
-        mountHeaderData(headerData);
-        mountBodyData(bodyData);
+  }
 
-      })
+  function buildSearchAndMountHistory() {
+    const searchHistory = getSearchHistory();
+    const searchHistoryList = document.getElementById('search-history');
+    searchHistoryList.innerHTML = '';
+    searchHistory.forEach((city) => {
+      const li = document.createElement('li');
+      li.className = 'list list-group-item';
+      li.innerHTML = city;
+      searchHistoryList.appendChild(li);
+    });
+  }
+
+  async function performSearch(cityName) {
+    const { lat, lon, error } = await searchForCity(cityName);
+    if (error) return alert(error);
+
+    const forecast = await getForecast(lat, lon);
+    const headerData = getHeaderData(cityName, forecast);
+    const bodyData = getBodyData(forecast);
+
+    mountHeaderData(headerData);
+    mountBodyData(bodyData);
+    buildSearchAndMountHistory();
+  }
+
+  // Event Listeners
+  $('#search-history').on('click', async function (e) {
+    const cityName = e.target.innerText;
+    performSearch(cityName);
   });
 
-//   https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+  $('#search-button').on('click', async function () {
+    const cityName = $('#search-input').val();
+    performSearch(cityName);
+  });
+
+  // page set up
+  buildSearchAndMountHistory();
+});
